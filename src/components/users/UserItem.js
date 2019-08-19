cb5535c9c98b457de6dcfd8e288a1a370de295bf
Avinav Singh <Avinav.Singh@BANCTECINDIA.IN>
2019-08-19 19:05:19 +0530

    Minor changes

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class UserItem extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div>
+                User Item
+            </div>
+        )
+    }
+}
+
+export default UserItem
